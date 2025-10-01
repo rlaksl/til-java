499a0c8c86ca32ac89a3af405298abd4d486376b
rlaksl <eehiuh@gmail.com>
2025-10-02 00:49:53 +0900

    첫 커밋: java-start 프로젝트 추가

---
+++ beginner/00java-start/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava { // HelloJava 클래스 범위 시작
+    public static void main(String[] args) { // main() 메서드 범위 시작
+        System.out.println("hello java");
+    } // main() 메서드 범위 끝
+} // HelloJava 클래스 범위 끝
+
+// 블록({})이 시작되고 끝날 때마다 들여쓰기가 적용되어 있음 들여쓰기는 코드를 쉽게 구분하고 이해하도록 도와줌
